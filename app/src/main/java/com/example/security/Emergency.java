15eb6605feb99d6c376a4efa4be88a1fde24037a
alicheaibx <41730625@students.liu.edu.lb>
2020-10-03 21:47:53 +0300

    ..........
    Signed-off-by: alicheaibx <41730625@students.liu.edu.lb>

--- app/src/main/java/com/example/security/Emergency.java
+++ app/src/main/java/com/example/security/Emergency.java
@@ -25,7 +25,7 @@ import java.util.ArrayList;
 
 public class Emergency extends AppCompatActivity {
     static String URL_notifications = "http://127.0.0.1/security_db/notificationpath.php";
-    String Url_case = "http://127.0.0.1/security_db/case.php";
+    static String URL_case = "http://127.0.0.1/security_db/case.php";
     TextView username;
     TextView phone;
     ImageButton sos;
@@ -49,7 +49,7 @@ public class Emergency extends AppCompatActivity {
         pn = getIntent().getExtras().getString("phone");
         cases = findViewById(R.id.sos);
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, Url_case, null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_case, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 JSONArray jsonArray = null;
@@ -84,6 +84,30 @@ public class Emergency extends AppCompatActivity {
     }
 
     private void emegency() {
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_notifications, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = response.getJSONArray("notifications");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String casename = jsonObject.optString("casename");
+                        caseList.add(casename);
+                        caseListAdapter = new ArrayAdapter<>(Emergency.this, android.R.layout.simple_spinner_dropdown_item);
+                        caseListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        cases.setAdapter(caseListAdapter);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
+            }
+
+        });
     }
 }
